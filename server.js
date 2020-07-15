a2cb803bc36957371c7482f4419581bf7c380043
Phill Peters <phillpeters1@gmail.com>
2020-07-15 13:39:07 -0600

    set up req auth

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ const app = express();
 app.use(morgan(`dev`));
 app.use(helmet());
 app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const bearerToken = req.get(`Authorization`);
+  const apiToken = process.env.API_TOKEN;
+  if (!bearerToken || bearerToken.split(` `)[1] !== apiToken) {
+    return res
+      .status(401)
+      .json({ error: `Unauthorized request` });
+  }
+  next();
+});
+
+app.get(`/moviedex`, (req, res) => {
+  res.json(MOVIEDEX);
+});
 
 const PORT = 8000;
 
